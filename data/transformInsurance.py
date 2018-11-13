527f94618a54cb161b0b4b30b30d36653795e32b
Matthew <chernoff.matthew@gmail.com>
2018-11-12 21:35:29 -0800

    additional model sizes and specification testing

--- data/transformInsurance.py
+++ data/transformInsurance.py
@@ -6,28 +6,40 @@ def yn_number(string):
     if(string == 'yes'):
         return 1
     elif(string == 'no'):
-        return -1
+        return 0
     else:
         raise Exception('Invalid boolean value: ' + string)
     
 
-def region_number(string):
+def region_vector(string):
     if(string == 'northeast'):
-        return 1
+        return [0,0,0,1]
     elif(string == 'northwest'):
-        return 2
+        return [0,0,1,0]
     elif(string == 'southeast'):
-        return 3
+        return [0,1,0,0]
     elif(string == 'southwest'):
-        return 4
+        return [1,0,0,0]
     else:
         raise Exception("Invalid region: " + string)
+
+def region_number(string):
+    if(string == 'northeast'):
+        return [1]
+    elif(string == 'northwest'):
+        return [2]
+    elif(string == 'southeast'):
+        return [3]
+    elif(string == 'southwest'):
+        return [4]
+    else:
+        raise Exception("Invalid region: " + string)    
 
 def sex_number(string):
     if(string == 'male'):
         return 1
     if(string == 'female'):
-        return -1
+        return 0
     else:
         raise Exception("Invalid gender: " + string)
 
@@ -36,9 +48,16 @@ def read_and_transform():
     with open('insurance.csv', 'r') as csvfile:
         reader = csv.reader(csvfile)
         header = next(reader)
-        rows = [[int(row[0]), sex_number(row[1]),float(row[2]),int(row[3]), \
-              yn_number(row[4]), region_number(row[5]),float(row[6])] \
-             for row in reader] \
+
+        rows = []
+        i = 0
+        for row in reader:
+            rows.append([int(row[0]), sex_number(row[1]),float(row[2]), \
+                       int(row[3]), yn_number(row[4])]) \
+                  
+            rows[i] += region_vector(row[5]) + [float(row[6])]
+            i += 1
+        
              
     return rows
 
--- model/nn-scikit.py
+++ model/nn-scikit.py
@@ -2,7 +2,10 @@ import pickle as pkl
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from sklearn.neural_network import MLPRegressor
+from sklearn.metrics import mean_squared_error
+from matplotlib import cm
 
 random.seed()
 
@@ -10,26 +13,72 @@ with open('../data/insurance.p','rb') as file:
     samples = pkl.load(file)
 
 training_epochs = 5000
+vlength = len(samples[0])-1
 
 random.shuffle(samples)
-features = [row[0:6] for row in samples]
-labels = [row[6] for row in samples]
+features = [row[0:vlength] for row in samples]
+labels = [row[vlength] for row in samples]
 
 labels = np.array(labels).astype(int)
 
+avg_label = np.mean(labels)
 
-perceptron = MLPRegressor(solver='lbfgs', alpha=1e-5,
-                               hidden_layer_sizes=(5,6),random_state=1)
+def run_regress(hidden_layers=(8),graph=False):
+    perceptron = MLPRegressor(solver='lbfgs', alpha=1e-5,
+                    hidden_layer_sizes=hidden_layers,
+                    random_state=1)
 
-perceptron.fit(features,labels)
+    
+    perceptron.fit(features,labels)
+    score = perceptron.score(features,labels)
 
-print(perceptron.score(features,labels))
+    predictions = perceptron.predict(features)
+    rmse = np.sqrt(mean_squared_error(predictions,labels))
+    print("Score: ",score)
+    print("RMSE: ", rmse)
+    if(graph):
+        fig, ax = plt.subplots()
+        ax.scatter(labels, predictions)
+        ax.plot([labels.min(), labels.max()], [labels.min(), labels.max()], 'k--', lw=3)
+        ax.set_xlabel('Measured')
+        ax.set_ylabel('Predicted')
+        plt.show()
+    return [score,rmse]
 
-predictions = perceptron.predict(features)
 
-fig, ax = plt.subplots()
-ax.scatter(labels, predictions)
-ax.plot([labels.min(), labels.max()], [labels.min(), labels.max()], 'k--', lw=3)
-ax.set_xlabel('Measured')
-ax.set_ylabel('Predicted')
-plt.show()
+def test_multilayer_sizes():    
+    scores = []
+    rmses = []
+    x1 = np.arange(2,10)
+    x2 = np.arange(2,10)
+    rmses = np.arange(8*8).reshape(8,8)
+
+    for i in x1:
+        for j in x2:
+            score,rmse = run_regress((i,j),False)
+            rmses[i-2][j-2] = rmse
+            
+    
+    rmses = np.asarray(rmses)   
+    plt.contour(x1,x2,rmses)
+    plt.title("RMS error")
+    plt.show()
+
+def test_layer_sizes():
+    x = np.arange(2,15)
+    rmses = np.arange(13)
+    scores = np.arange(13)
+
+    for i in x:
+        score,rmse = run_regress((i), False)
+        rmses[i-2] = rmse
+        scores[i-2] = score
+    
+    fig, ax1 = plt.subplots()
+    plt.title("RMS error")
+    plt.plot(x,rmses)
+    plt.show()
+
+#test_multilayer_sizes();
+#test_layer_sizes();
+run_regress((8,5))
