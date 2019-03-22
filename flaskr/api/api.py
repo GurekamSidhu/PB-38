e0b8e6c11c232d4218ba13f0efc74c08197f9335
Logan Ennis <loganennis1996@gmail.com>
2019-03-22 10:20:50 -0700

    switched to using runpy

--- flaskr/api/api.py
+++ flaskr/api/api.py
@@ -5,7 +5,7 @@ from webargs import fields, validate							# To validate arguments
 
 import pickle as pkl
 import os
-import importlib
+import runpy
 import datetime
 
 # Register blueprint
@@ -19,7 +19,7 @@ top_level = '/dynprice'
 # model = ReceiptsModel()
 script_file = home + top_level + '/script/receipts_model.py'
 model_file = home + top_level + '/bin/receipts-model.pkl'
-importlib.import_module(script_file)
+runpy.run_path(file_path=script_file)
 model = pkl.load(open(model_file, 'rb'))
 print(model)
 
